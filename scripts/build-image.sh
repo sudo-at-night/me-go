4aac351482a0003b29254fd5918d1a331ab04b55
Patryk Mazur <patytmaz@gmail.com>
2020-02-06 19:08:02 +0100

    Create deployment scripts

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -4,5 +4,6 @@ docker build -t me-go -f docker/Dockerfile .
 
 if ! [ -z "$1" ] || [ "$1" == "--save" ]; then
     echo "Saving the image as '/me-go.tar'";
-    docker save -o me-go.tar me-go
+    mkdir -p files/image
+    docker save -o files/image/me-go.tar me-go
 fi
